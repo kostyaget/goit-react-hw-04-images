568c29a53fa218931d15e9f49685bf94126d4bba
kostyaget <progogyd@gmail.com>
2022-07-02 15:50:40 +0300

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,20 +7,17 @@ import { Overlay, ModalContainer, CloseButton, LargeImg } from './Modal.styled';
 
 const modalRoot = document.getElementById('modal-root');
 
-export default function Modal({ largeImageURL, alt, onClose }) {
-  useEffect(() => {
-    window.addEventListener('keydown', onKeyDown);
+export default function Modal({ largeImageURL, alt, onClose,}) {
+  useEffect((onKeyDown) => {
+    
+    window.addEventListener('keydown',onKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', onKeyDown);
+      window.removeEventListener('keydown',onKeyDown);
     };
   });
 
-  const onKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
+  
 
   const onBackdropClick = e => {
     if (e.currentTarget === e.target) {
